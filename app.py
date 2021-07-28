98effcd205dfecc5d4c6918a7f318bbff08ff3b9
Serge <sergey@batalov.email>
2021-07-28 11:30:04 +0100

    всё работает

--- app.py
+++ app.py
@@ -1,56 +1,9 @@
-import json
-
-import requests
 import telebot
-from config import TOKEN, ACCESS
 
-class ConverterExceptions(Exception):
-    pass
-
-
-currencies = {
-    "доллар": "USD",
-    "евро": "EUR",
-    "рубль": "RUB",
-}
-def cross_currency(cur1,cur2):
-    return cur1/cur2
-
+from config import currencies, TOKEN
+from extensions import Exchange, ConverterExceptions
 
 bot = telebot.TeleBot(TOKEN)
-type_of_data = 'latest'
-api_version = 'v1'
-base_url = 'api.exchangeratesapi.io'
-protocol = 'http'
-
-address = f'{protocol}://{base_url}/{api_version }/{type_of_data}?access_key={ACCESS}&base={currencies["евро"]}&symbols={currencies["рубль"]},{currencies["доллар"]}'
-
-class Exchange:
-
-    @staticmethod
-    def process_request(currency_from, currency_to):
-        received_data = requests.get(address)
-        formatted_data = json.loads(received_data.content)
-        print(formatted_data)
-        if currencies[currency_from] == "EUR":
-            currency_from = 1
-            currency_to = formatted_data["rates"][currencies[currency_to]]
-        elif currencies[currency_to] == "EUR":
-            currency_to = 1
-            currency_from = formatted_data["rates"][currencies[currency_from]]
-        else:
-            currency_to = formatted_data["rates"][currencies[currency_to]]
-            currency_from = formatted_data["rates"][currencies[currency_from]]
-
-        conversion = float(currency_to)/float(currency_from)
-        return conversion
-
-
-
-# print(formatted_data["rates"]["RUB"])
-#
-# print(received_data.text)
-# print(formatted_data)
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
@@ -61,7 +14,7 @@ def help(message: telebot.types.Message):
     bot.send_message(message.chat.id, "Привет " + message.chat.first_name)
     bot.send_message(message.chat.id, text)
 
-@bot.message_handler(commands=['value'])
+@bot.message_handler(commands=['values'])
 def function_name(message: telebot.types.Message):
     text = "Доступные для конвертации валюты"
     for key in currencies.keys():
@@ -70,36 +23,29 @@ def function_name(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text'])
 def reply_to_user(message: telebot.types.Message):
-    received = message.text.split(" ")
-
-    if len(received) > 3:
-        raise ConverterExceptions("Введено боллее 3 параметров")
-
-    currency_from, currency_to, how_much = received
-
-    if currency_from == currency_to:
-        raise ConverterExceptions("Вы идиот?")
-
     try:
-        control = currencies[currency_to]
-    except KeyError:
-        raise ConverterExceptions(f"Несуществующая валюта: {control}")
+        received = message.text.split(" ")
 
-    try:
-        control = currencies[currency_from]
-    except KeyError:
-        raise ConverterExceptions(f"Несуществующая валюта: {control}")
+        if len(received) != 3:
+            raise ConverterExceptions("Неверное количество параметров. Должно быть 3.")
 
-    try:
-        control = float(how_much)
-    except KeyError:
-        raise ConverterExceptions(f"Непонятно сколько конвертировать: {control}")
+        currency_from, currency_to, how_much = received
 
-    reply = Exchange.process_request(currency_from, currency_to)
-    text = f"Запрошенный курс из {currency_from} в {currency_to}:\n" \
-           f"За {how_much} {currency_from} Вы получите {reply} {currency_to}"
+        reply = Exchange.get_price(currency_from, currency_to, how_much)
 
-    bot.send_message(message.chat.id, text)
+
+    except ConverterExceptions as e:
+        bot.send_message(message.chat.id, f"Произшла ошибка:\n{e}")
+
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Что-то пошло не так:\n{e}")
+
+    else:
+
+        text = f"Запрошенный курс из {currency_from} в {currency_to}:\n" \
+               f"За {how_much} {currency_from} Вы получите {reply} {currency_to}"
+
+        bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
 
---
+++ extensions.py
@@ -0,0 +1,46 @@
+import json
+import requests
+from config import *
+
+class ConverterExceptions(Exception):
+    pass
+
+class Exchange:
+
+    @staticmethod
+    def get_price(currency_from, currency_to, how_much):
+
+        if currency_from == currency_to:
+            raise ConverterExceptions(f"А смысл?")
+
+        try:
+            control = currencies[currency_from]
+        except KeyError:
+            raise ConverterExceptions(f"Несуществующая валюта: {currency_from}")
+
+        try:
+            control = currencies[currency_to]
+        except KeyError:
+            raise ConverterExceptions(f"Несуществующая валюта: {currency_to}")
+
+        try:
+            control = float(how_much)
+        except KeyError:
+            raise ConverterExceptions(f"Непонятно сколько конвертировать: {how_much}")
+
+        address = f'{protocol}://{base_url}/{api_version}/{type_of_data}?access_key={ACCESS}&base={currencies["евро"]}&symbols={currencies["рубль"]},{currencies["доллар"]}'
+        received_data = requests.get(address)
+        formatted_data = json.loads(received_data.content)
+
+        if currencies[currency_from] == "EUR":
+            currency_from = 1
+            currency_to = formatted_data["rates"][currencies[currency_to]]
+        elif currencies[currency_to] == "EUR":
+            currency_to = 1
+            currency_from = formatted_data["rates"][currencies[currency_from]]
+        else:
+            currency_to = formatted_data["rates"][currencies[currency_to]]
+            currency_from = formatted_data["rates"][currencies[currency_from]]
+
+        conversion = (float(currency_to)/float(currency_from))*float(how_much)
+        return conversion
