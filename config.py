48ff7d2187cb02ee3ec13158d58c818b20288d9b
Serge <sergey@batalov.email>
2021-07-28 11:55:23 +0100

    всё работает комменты добавлены добавил конфиг в гит

---
+++ config.py
@@ -0,0 +1,14 @@
+
+TOKEN: str = '1902504290:AAFXYS3ocRxFnxf5gvG-8M68usGBzZvtwts'
+ACCESS: str = '9bbff0c9e910c8a53afa03098a702b4b'
+
+type_of_data: str = 'latest'
+api_version: str = 'v1'
+base_url: str = 'api.exchangeratesapi.io'
+protocol: str = 'http'
+
+currencies = {
+    "доллар": "USD",
+    "евро": "EUR",
+    "рубль": "RUB",
+}
